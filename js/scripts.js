960c683fd98256e91dc0b7ef0eb448e5427cf310
webdev <webdev.solid@gmail.com>
2025-01-20 22:15:25 +0100

    added listining on resize window

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,8 @@
         const canvas = document.querySelector("canvas");
         const crc = canvas.getContext('2d');
 
-        const canvasWidth = canvas.width = window.innerWidth;
-        const canvasHeight = canvas.height = window.innerHeight;
-        const canvasOffsetTop = canvas.offsetTop;
-        const canvasOffsetLeft = canvas.offsetLeft;
+        let canvasWidth = canvas.width = window.innerWidth;
+        let canvasHeight = canvas.height = window.innerHeight;
 
         const mousePosition = {
             x: undefined,
@@ -18,6 +16,11 @@
             mousePosition.y = event.y;
 
         });
+
+        window.addEventListener("resize", () => {
+            canvasWidth = canvas.width = window.innerWidth;
+            canvasHeight = canvas.height = window.innerHeight;
+        })
 
         class Circle {
             constructor(x, y, radious) {
@@ -57,7 +60,7 @@
 
                 let dX = this.x - mousePosition.x;
                 let dY = this.y - mousePosition.y;
-                const distance = Math.sqrt(dX**2 + dY**2)
+                const distance = Math.sqrt(dX ** 2 + dY ** 2)
                 if (distance <= 100 && this.radious <= this.radiousInit + this.radiousMax) {
                     this.radious += this.radiousAcceleration;
                 } else if (this.radious >= this.radiousInit && distance > 100) {
@@ -72,7 +75,7 @@
             circles[i] = new Circle(canvasWidth, canvasHeight, 10);
         }
 
-        const fps = 1000 / 120;
+        const fps = 1000 / 520;
         let time = 0
         let t = 0;
 
